61fe6326183322af3e02204fa1c342733c6fd486
yura12042002 <122212831+yura12042002@users.noreply.github.com>
2023-10-14 00:37:43 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import InformationAboutUser from "./components/informationAboutUser/InformationAboutUser";
+
+function App() {
+  return (
+    // <div className="contain">
+    //   {data.map((el) => 
+    //     <Work
+    //       id={el.id}
+    //       company={el.company}
+    //       logo={el.logo}
+    //       isNew={el.new}
+    //       featured={el.featured}
+    //       position={el.position}
+    //       role={el.role}
+    //       level={el.level}
+    //       postedAt={el.postedAt}
+    //       contract={el.contract}
+    //       location={el.location}
+    //       languages={el.languages}
+    //       tools={el.tools}
+    //     />
+    //   ))}
+    // </div>
+    // <Tabs/>
+    // <Asked/>
+    // <Asked2/>
+    // <Todo/>
+    // <Calcul/>
+    // <Background/>
+    // <Words/>
+    // <List/>
+    // <Questionnaire/>
+    // <Cigarettes/>
+    // <List/>
+    // <MiniCalculate/>
+    // <Consumption />
+    // <FarAway/>
+    // <SquareCount/>
+    // <Coffee />
+    // <SmileFace/>
+    // <FirstWorkLocalStorege/>
+    // <RandomUser/>
+    // <TheAdressBook />
+    // <UsingCastomHooks/>
+    // <Joke/>
+    // <Todo />
+    // <UserPersonalData/>
+    <InformationAboutUser />
+    // <CirclesAndSquares/>
+  );
+}
+
+export default App;
---
+++ src/data/QuestionsData.js
@@ -0,0 +1,14 @@
+export const questions = [
+    {
+        question: 'Who built this Javascriptless accordion demo?',
+        answer: 'If you got your foreign passport or have just applies for it, its time to think about buying airplane tickets. You can buy tickets at ticket offices or book flights online. Moreover, dont forget to get travel insurance. As a rule, its required (its obligatory).'
+    },
+    {
+        question:'Why not just use Javascript?',
+        answer: 'Travelling to far countries is always a thrilling and interesting adventure. Heading for the other end of the world, its impossible to go without such an aircraft as “a plane”. But before boarding a plane, one must book a seat in advance and go though other different formalities.'
+    },
+    {
+        question: 'Can I Just copy and paste this code for my own project?',
+        answer: 'The tickets are on hand, the insurance is in a pocket, a suitcase is packed – its time to go to the airport. You should arrive at the airport two hours earlier whether you fly in business class or in economy class.'
+    }
+]
